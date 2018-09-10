89b9dd92197252ae3b27fef5d7e0d01151ffb9c3
develargus <develargus@gmail.com>
2018-09-10 23:57:32 +0600

    Prod Environment Configuration

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,14 +3,30 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=itopshop',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
         ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'messageConfig' => [
+                'from' => ['support@itopshop.com' => 'iTopShop']
+            ],
+        ],
+        'robokassa' => [
+            'class' => '\robokassa\Merchant',
+            'baseUrl' => 'https://auth.robokassa.ru/Merchant/Index.aspx',
+            'sMerchantLogin' => '',
+            'sMerchantPass1' => '',
+            'sMerchantPass2' => '',
         ],
     ],
 ];
--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,3 +1,11 @@
 <?php
 return [
+    'cookieValidationKey' => '',
+    'cookieDomain' => '.itopshop.ru',
+    'frontendHostInfo' => 'http://itopshop.ru',
+    'backendHostInfo' => 'http://backend.itopshop.ru',
+    'staticHostInfo' => 'http://static.itopshop.ru',
+    'mailChimpKey' => '',
+    'mailChimpListId' => '',
+    'smsRuKey' => '',
 ];
