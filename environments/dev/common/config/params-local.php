d13aa5b044af6246cb38d27d09ff76808e45db91
develargus <develargus@gmail.com>
2018-09-10 23:45:04 +0600

    Dev Environment Configuration

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,11 @@
 <?php
 return [
+    'cookieValidationKey' => '',
+    'cookieDomain' => '.itopshop.dev',
+    'frontendHostInfo' => 'http://itopshop.dev',
+    'backendHostInfo' => 'http://backend.itopshop.dev',
+    'staticHostInfo' => 'http://static.itopshop.dev',
+    'mailChimpKey' => '<your-key>',
+    'mailChimpListId' => '',
+    'smsRuKey' => '',
 ];
